c0402aa3a2728aba74d94a6a5c5408165403d966
XTW <65795859+XT8SS@users.noreply.github.com>
2023-12-12 18:22:51 -0700

    Delete src directory

--- src/lib/stores.js
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store";
-
-let selectedEqData = writable({}), buildStats = writable({}), selectedGearData = writable({});
-
-export {selectedEqData, buildStats, selectedGearData};
